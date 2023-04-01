b3dfc2ab9b54f571a838b36d138bca2edffce21e
marcusziade <marcus.ziade@ouraring.com>
2023-04-01 21:17:56 +0300

    Add loading indicator while getting links from search URL

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 
 	"github.com/atotto/clipboard"
 )
@@ -18,9 +20,38 @@ func run() error {
 	if err != nil {
 		return fmt.Errorf("error reading clipboard: %w", err)
 	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	stopLoading := make(chan bool)
+	go func() {
+		defer wg.Done()
+		loadingIndicator(stopLoading)
+	}()
+
 	err = GetLinks(searchURL)
 	if err != nil {
 		return fmt.Errorf("error getting links: %w", err)
 	}
+
+	stopLoading <- true
+	wg.Wait()
+
 	return nil
+}
+
+func loadingIndicator(stop chan bool) {
+	chars := []string{"-", "\\", "|", "/"}
+	i := 0
+	for {
+		select {
+		case <-stop:
+			fmt.Print("\r")
+			return
+		default:
+			fmt.Printf("\rLoading %s", chars[i])
+			i = (i + 1) % len(chars)
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
 }
